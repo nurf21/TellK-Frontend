40fab134ed829cbc796f68cbdd83277bb06178e2
nurf21 <nurfauzangiri@gmail.com>
2020-09-23 18:14:18 +0700

    :lipstick: Add Login page

--- src/main.js
+++ src/main.js
@@ -1,12 +1,27 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import * as VueGoogleMaps from 'vue2-google-maps'
+import VueGeolocation from 'vue-browser-geolocation'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'YOUR_KEY',
+    libraries: 'places'
+  }
+})
+Vue.use(VueGeolocation)
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
---
+++ src/views/Login.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="login">
+    <b-container class="login-c">
+      <h1>Login</h1>
+      <p class="welcome">Hi, welcome back!</p>
+      <b-form @submit.prevent="onSubmit">
+        <b-form-group id="input-group-1" label="Email" label-for="input-1">
+          <b-form-input
+            id="input-1"
+            type="email"
+            required
+            placeholder="Enter email"
+          ></b-form-input>
+        </b-form-group>
+
+        <b-form-group id="input-group-2" label="Password" label-for="input-2">
+          <b-form-input
+            id="input-2"
+            type="password"
+            required
+            placeholder="Enter password"
+          ></b-form-input>
+        </b-form-group>
+
+        <p class="forgot">Forgot password?</p>
+
+        <b-button type="submit" class="login-btn">Login</b-button>
+        <p class="alt-login">Login with</p>
+        <b-button class="google-btn">Google</b-button>
+        <p class="sign-up">Don't have an account? <span>Sign Up</span></p>
+      </b-form>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login'
+}
+</script>
+
+<style scoped src="../assets/css/style.css"></style>
